15aa2970f0759b73dde36269d158436ab9015c3d
RationalAsh <ashwinnarayan1994@gmail.com>
2015-06-25 11:11:15 +0800

    Made code more general

--- xbeerecv.c
+++ xbeerecv.c
@@ -4,11 +4,16 @@
 
 
 //For XBEE-1
-#define XB1ADDR "192.168.1.151"
-#define XB2ADDR "192.168.1.152"
+#define XB0IP "192.168.1.150"
+#define XB1IP "192.168.1.151"
+#define XB2IP "192.168.1.152"
+#define XB3IP "192.168.1.153"
+#define XB4IP "192.168.1.154"
+#define XB5IP "192.168.1.155"
+#define XB6IP "192.168.1.156"
 #define LOCAL "127.0.0.1"
 #define TESTPORT 5000
-#define XB1PORT 9750
+#define XBPORT 9750
 #define MSGBUFSIZE 1024
 #define DOF 3
 #define SENSORS 7
@@ -21,12 +26,13 @@ char reply1[MSGBUFSIZE];
 char reply2[MSGBUFSIZE];
 double sensor_readings[SENSORS][DOF];
 double quaternions[SENSORS][4];
+int sensormask[SENSORS] = {0,1,1,0,0,0,0};
 
 //Structs for IO waiting
 fd_set master;
 fd_set keypress;
 
-void read_data(int *sockets, int num_sensors);
+void read_data(int *sockets);
 void update_ypr(char *data_str, int sensor_number);
 void print_readings();
 void print_quaternions();
@@ -35,38 +41,72 @@ void connect_xbees();
 
 int main(int argc, char** argv)
 {
+    int i, j;
     int xbsocks[SENSORS];
+    char **xbips;
+
+    //Array of IPS
+    xbips = (char**)malloc(SENSORS*sizeof(char*));
+    for(i=0;i<SENSORS;i++)
+	xbips[i] = (char *)malloc(16*sizeof(char));
+
+    xbips[0] = XB0IP;
+    xbips[1] = XB1IP;
+    xbips[2] = XB2IP;
+    xbips[3] = XB3IP;
+    xbips[4] = XB4IP;
+    xbips[5] = XB5IP;
+    xbips[6] = XB6IP;	    
+    
+    //Value for the timeout
     struct timeval timeout;
     timeout.tv_sec = TIMEOUT;
     timeout.tv_usec = 0;
 
 
     //Connect to the XBees using TCP
-    xbsocks[0] = establishConnection(XB1ADDR, XB1PORT);
-    xbsocks[1] = establishConnection(XB2ADDR, XB1PORT);
+    //xbsocks[0] = establishConnection(XB0IP, XBPORT);
+    //xbsocks[1] = establishConnection(XB1IP, XBPORT);
+    //xbsocks[2] = establishConnection(XB2IP, XBPORT);
+    //xbsocks[3] = establishConnection(XB3IP, XBPORT);
+    //xbsocks[4] = establishConnection(XB4IP, XBPORT);
+    //xbsocks[5] = establishConnection(XB5IP, XBPORT);
+    //xbsocks[6] = establishConnection(XB6IP, XBPORT);
+
+    for(i=0;i<SENSORS;i++)
+    {
+	if(sensormask[i])
+	    xbsocks[i] = establishConnection(xbips[i], XBPORT);
+    }
 
     //Set up stdin with select
     int fd_stdin = fileno(stdin);
 
     //Clearing the sensor readings
-    int i, j;
+    
     for(i=0;i<SENSORS;i++)
 	for(j=0;j<DOF;j++)
 	    sensor_readings[i][j] = 0;
 
-    tcpWrite(xbsocks[0], "aaa");
-    tcpWrite(xbsocks[1], "aaa");
+    for(i=0;i<SENSORS;i++)
+    {
+	if(sensormask[i])
+	    tcpWrite(xbsocks[i], "aaa");
+    }
+    
 
     while(1)
     {
 	//Setting up struct for TCP
 	FD_ZERO(&master);
-	FD_SET(xbsocks[0], &master);
-	FD_SET(xbsocks[1], &master);
+	//FD_SET(xbsocks[0], &master);
+	//FD_SET(xbsocks[1], &master);
 
-	//Setting up struct for stdin
-	FD_ZERO(&keypress);
-	FD_SET(fd_stdin, &keypress);
+	for(i=0;i<SENSORS;i++)
+	{
+	    if(sensormask[i])
+		FD_SET(xbsocks[i], &master);
+	}
 
 	//Wait for incoming data to be available
 	int num_ready = select(FD_SETSIZE, &master, NULL, NULL, &timeout);
@@ -76,7 +116,7 @@ int main(int argc, char** argv)
 	{
 	    //Reads the data, converts it to quaternions and puts it 
 	    //in the quaternions[SENSORS][4] array.
-	    read_data(xbsocks, 2);
+	    read_data(xbsocks);
 	    //print_readings();
 	    print_quaternions();
 	}
@@ -159,19 +199,20 @@ void update_ypr(char *data_str, int sensor_number)
     }
 }
 
-void read_data(int *sockets, int num_sensors)
+void read_data(int *sockets)
 {
     int i = 0;
 
-    for(i=0; i<num_sensors; i++)
+    for(i=0; i<SENSORS; i++)
     {
-	if(FD_ISSET(sockets[i], &master))
+	if(sensormask[i]){
+	if((FD_ISSET(sockets[i], &master)))
 	{
 	    tcpRead(sockets[i], reply);
 	    printf("Xbee%d: %s\n", i, reply);
 	    char *temp = strdup(reply);
 	    update_ypr(temp, i);
 	    update_quaternion(i);
-	}
+	}}
     }
 }
